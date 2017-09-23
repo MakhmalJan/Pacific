1afdd4d9d7498d8480f5df4a1e37185bb90ef49c
MakhmalJan <32045672+MakhmalJan@users.noreply.github.com>
2017-09-23 00:36:11 -0700

    Add files via upload

--- js/main2.js
+++ js/main2.js
@@ -3,16 +3,25 @@
   // Fade in the modal (.modal-background)
 //$('#drinks').hide();
 
+
+$('.hamburger').on('click', function() {
+  $('.nav-links').addClass('active');
+});
+
+$('.close').on('click', function() {
+  $('.nav-links').removeClass('active');
+
+});
+
 $('#make-reservation').on('click', function() {
   $('.reservation-background').fadeIn(300);
 });
 
-// When the user clicks the close button (#close)
-  // Fade out the modal (.modal-background)
+    // When the user clicks the close button (#close)
+      // Fade out the modal (.modal-background)
 $('#close').on('click', function() {
   $('.reservation-background').fadeOut(300);
 });
-
 
 $('#images').slick({
   slidesToShow: 1,
@@ -21,23 +30,21 @@ $('#images').slick({
   autoplaySpeed: 2000,
 });
 
-
-
-// When the user clicks .show-drinks
+    // When the user clicks .show-drinks
 $('.show-drinks').on('click', function (event) {
   event.preventDefault();
 
-  // Hide #brunch and #dinner
+      // Hide #brunch and #dinner
   $('#show-brunch').hide();
   $('#show-dinner').hide();
 
-  // Fade in #drinks
+      // Fade in #drinks
   $('#show-drinks').fadeIn(300);
-})
+});
 
 
 
-// When the user clicks .show-drinks
+    // When the user clicks .show-drinks
 $('.show-brunch').on('click', function (event) {
   event.preventDefault();
 
@@ -45,11 +52,11 @@ $('.show-brunch').on('click', function (event) {
   $('#show-dinner').hide();
 
   $('#show-brunch').fadeIn(300);
-})
+});
 
 
 
-// When the user clicks .show-drinks
+    // When the user clicks .show-drinks
 $('.show-dinner').on('click', function (event) {
   event.preventDefault();
 
@@ -57,4 +64,4 @@ $('.show-dinner').on('click', function (event) {
   $('#show-drinks').hide();
 
   $('#show-dinner').fadeIn(300);
-})
+});
